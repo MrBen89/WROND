16e6113edc03fcc4a7884570ae955ddec54be224
Liam Gingell <elgee95@gmail.com>
2025-02-06 21:36:35 +0900

    Created routes for kanji, puzzles, user_profiles and upgrades

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :kanji, only: [:index, :show]
+  resources :puzzles, only: [:create]
+  resources :user_profiles, only: [:show, :update]
+  resources :upgrades, only: [:show]
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
