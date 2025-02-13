ca4010ece60ee0546987b7f6293e0e0cba0c693b
Julian Cass <j.cass@outlook.com>
2025-02-13 14:01:33 +0900

    Updated user profiles seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :user_profiles
+  has_one :user_profile, dependent: :destroy
   has_many :unlocks
   has_many :puzzles
+  accepts_nested_attributes_for :user_profile
 end
