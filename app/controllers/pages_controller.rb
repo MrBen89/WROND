4d52698b39017b4e23866ae42a0762ae8ea39191
Ben Pearson <benpearson61@googlemail.com>
2025-02-22 18:26:55 +0900

    oh dear

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:intro, :index]
 
   def home
+  end
+
+  def intro
+    redirect_to home_path if user_signed_in?
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+
+  root to: "pages#intro"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'kanji/index', to: 'kanji#index'
-
+  get '/home', to: 'pages#home'
 
   resources :kanji, only: [:index, :show]
   resources :puzzles, only: [:create]
