440ab9ea536e1779b5384d0847f57b1addf23eb7
Miyabi Tasaki <m.tasaki29@gmail.com>
2025-02-27 01:58:24 +0900

    added tutorial gifs and code but somehow they not working

--- app/javascript/controllers/mode_controller.js
+++ app/javascript/controllers/mode_controller.js
@@ -1,4 +1,3 @@
-// app/javascript/controllers/mode_selector_controller.js
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
@@ -10,29 +9,21 @@ export default class extends Controller {
   }
 
   setMode(event) {
-    console.log(event);
     const mode = event.target.dataset.mode;
-    console.log(mode);
     const isActive = event.target.classList.contains("active");
     let target = document.querySelector(`[data-mode="${mode}"]`);
-    console.log(target);
 
-    // If the mode is already active, remove it
     if (isActive) {
       target.classList.remove("active");
       this.selectedModeTarget.value = "";
       this.playButtonTarget.disabled = true;
       this.modeDescriptionTarget.innerHTML = "Choose a mode!";
     } else {
-      // Deactivate all other buttons and activate the clicked one
       this.buttonTargets.forEach((button) => button.classList.remove("active"));
       target.classList.add("active");
       this.selectedModeTarget.value = mode;
-
-      // Enable the play button
       this.playButtonTarget.disabled = false;
 
-      // Update the mode description
       const descriptions = {
         kanji: "Kanji mode allows you to practice individual kanji.<br>The classic mode!",
         story: "Try and practice the kanji you've learnt by making<br>SENTENCES!"
@@ -40,8 +31,12 @@ export default class extends Controller {
       this.modeDescriptionTarget.innerHTML = descriptions[mode];
     }
   }
+
   toggleTutorial() {
     const tutorialContent = document.getElementById("tutorial-content");
-    tutorialContent.classList.toggle("active"); // Toggle the active class to slide in/out
+    const overlay = document.getElementById("overlay");
+
+    tutorialContent.classList.toggle("active"); // Slide in/out
+    overlay.classList.toggle("active"); // Show/hide overlay
   }
 }
