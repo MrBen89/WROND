5e30b46c4d74a3c3f0dcf5134744e889adab52cb
Liam Gingell <elgee95@gmail.com>
2025-03-01 19:28:13 +0900

    It worksgit add .git add .! Wrondarou's wisdom fades in using stimulus and magic

--- app/javascript/controllers/wrondarou_controller.js
+++ app/javascript/controllers/wrondarou_controller.js
@@ -1,16 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["hint"];
+  static targets = ["hint", "wrondarou"];
 
   connect() {
-  setTimeout(() => {
-    document.querySelector(".game-gif-bottom").addEventListener("click", this.toggle.bind(this));
-    document.querySelector(".ask-wrondarou-btn").classList.remove("hidden");
-  }, 5000);
+    this.clickable = false;
+    setTimeout(() => {
+      document.querySelector(".ask-wrondarou-btn").classList.remove("hidden");
+      this.clickable = true;
+    }, 5000);
   }
 
-  toggle() {
-    document.querySelector(".words-of-wisdom").classList.remove("hidden");
+  appear() {
+    if (!this.clickable) return;
+
+    this.hintTarget.classList.add("show");
+    // this.hintTarget.classList.style.transition = "opacity 1s";
   }
 }
